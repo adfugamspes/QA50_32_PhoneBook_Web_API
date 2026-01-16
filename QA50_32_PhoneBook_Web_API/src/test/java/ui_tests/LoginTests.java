ca564310b96432cb508c0a337d256c5e11144622
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-01-16 11:59:55 +0200

    Initial commit

---
+++ QA50_32_PhoneBook_Web_API/src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,16 @@
+package ui_tests;
+
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class LoginTests extends AppManager {
+
+    @Test
+    public void loginPositiveTest () {
+        System.out.println("firstTest");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+    }
+
+}
